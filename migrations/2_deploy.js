f469ecd4a6cbb4d28d1fdde0d140e0584d247d59
brmrush <olgun2107@gmail.com>
2022-08-03 22:19:10 +0300

    fix

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -2,18 +2,9 @@ const RushToken = artifacts.require("RushToken");
 const RushStake = artifacts.require("RushStake");
 
 module.exports = async function(deployer) {
-	//deploy Token
 	await deployer.deploy(RushToken)
-
-	//assign token into variable to get it's address
 	const rushToken = await RushToken.deployed()
-	
-	//pass token address for dBank contract(for future minting)
 	await deployer.deploy(RushStake, rushToken.address)
-
-	//assign dBank contract into variable to get it's address
 	const rushStake = await RushStake.deployed()
-
-	//change token's owner/minter from deployer to dBank
 	await rushToken.transferMinterRole(rushStake.address)
 };
